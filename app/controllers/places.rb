9ff45ec87abb027c8c9b6732e688ff7d2769fd1c
Tomy8s <tomy8s@hotmail.com>
2016-11-01 17:13:03 +0000

    added listings page

--- app/controllers/places.rb
+++ app/controllers/places.rb
@@ -1,4 +1,18 @@
 class Airbminusb < Sinatra::Base
 
+  get '/places/new' do
+    erb :'places/new'
+  end
 
+  post '/places/new' do
+    params[:user_id] = 1#session[:user_id]
+    @place = Place.create(params)
+    redirect '/places/listings'
+    flash[:notice] << "#{@place.name} has been added."
+  end
+
+  get '/places/listings' do
+    @places = Place.all
+    erb :'places/listings'
+  end
 end
