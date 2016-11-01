00200068318ca955b3078a3b7ea3bf83c531fc91
Tomy8s <tomy8s@hotmail.com>
2016-11-01 12:54:20 +0000

    sign_in works

--- app/app.rb
+++ app/app.rb
@@ -18,6 +18,10 @@ class Airbminusb < Sinatra::Base
     erb :index
   end
 
+  get '/sessions/sign_in' do
+    erb :sign_in
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
