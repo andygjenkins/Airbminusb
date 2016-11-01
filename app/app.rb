8ea1d4911b9c2968e35dc7ad6518c77f2820b158
Tomy8s <tomy8s@hotmail.com>
2016-11-01 14:11:54 +0000

    fixed merge conflict

--- app/app.rb
+++ app/app.rb
@@ -12,16 +12,3 @@ require_relative 'controllers/places'
 require_relative 'controllers/requests'
 require_relative 'controllers/sessions'
 require_relative 'controllers/user'
-
-class Airbminusb < Sinatra::Base
-  get '/' do
-    erb :index
-  end
-
-  get '/sessions/sign_in' do
-    erb :sign_in
-  end
-
-  # start the server if ruby file executed directly
-  run! if app_file == $0
-end
