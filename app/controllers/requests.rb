505eeede4f0293e4f3c2b4994e00154fc77bb693
andy jenkins <andygjenkins@gmail.com>
2016-11-02 15:36:47 +0000

    Add requests functionality to be styled

--- app/controllers/requests.rb
+++ app/controllers/requests.rb
@@ -12,7 +12,14 @@ class Airbminusb < Sinatra::Base
   get '/requests' do
     @requests_made = Booking.requests_made(session[:user_id])
     @requests_received = Booking.requests_received(session[:user_id])
+    erb :'requests/index'
   end
 
+  post '/requests/confirm' do
+    @confirmed_booking = Booking.get(params[:id])
+    @confirmed_booking.update(confirmed: true)
+    flash.now[:notices] = ["Booking Confirmed!"]
+    redirect to '/requests'
+  end
 
 end
