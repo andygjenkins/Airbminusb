68e7450b9b9e5f02f1989572c0cb4b34e4dfe649
andy jenkins <andygjenkins@gmail.com>
2016-11-02 10:35:54 +0000

    Change html styling  and flash partials

--- app/controllers/requests.rb
+++ app/controllers/requests.rb
@@ -1,4 +1,10 @@
 class Airbminusb < Sinatra::Base
 
+  post 'requests/new' do
+    @booking = Booking.create(params)
+    redirect to '/requests'
+    flash[:notice] << "You have requested to stay at #{@booking.place.name} on #{@booking.date}"
+  end
+
 
 end
