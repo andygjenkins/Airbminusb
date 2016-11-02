aa0552357c2e4ff1ed5357c2da7f266f8a5e161f
david anderson <d.a.m.anderson@gmail.com>
2016-11-02 19:02:29 +0000

    fixed date picker to post correct date format

--- app/public/datepicker.js
+++ app/public/datepicker.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
-  var array = ["2016-11-14","2016-11-15","2016-11-16"]
+  var array = ["2016-11-11","2016-12-11","2016-13-11"]
 
   $('#datepicker').datepicker({
    beforeShowDay: function(date){
-       var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+       var string = jQuery.datepicker.formatDate('yy-dd-mm', date);
        return [ array.indexOf(string) == -1 ]
    }
   });
 
    $('#request_booking').click(function() {
-     $('#input_date').attr('value', $('#datepicker').val());
+     $('#input_date').attr('value', $('#datepicker').datepicker( "getDate"));
    });
 });
