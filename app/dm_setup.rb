41f92af8e8d177fa2950072c5ae6fd4126e77011
Stephen Gregory <stejgregory@gmail.com>
2016-11-03 16:30:03 +0000

    Last push for heroku

--- app/dm_setup.rb
+++ app/dm_setup.rb
@@ -6,6 +6,6 @@ require_relative 'models/Place'
 require_relative 'models/Booking'
 require_relative 'models/send_recover_link'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres:///airbminusb_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/airbminusb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
